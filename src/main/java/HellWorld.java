6f36c43a388c2e596e24f6ee115f98559aeb80c7
Deaglusha <deaglusha@gmail.com>
2022-04-03 22:41:35 +0300

    Test GitHub
    Test GitHub

--- src/main/java/HellWorld.java
+++ src/main/java/HellWorld.java
@@ -4,5 +4,7 @@ public class HellWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is our second commit");
+
+        System.out.println("Test GitHub");
     }
 }
