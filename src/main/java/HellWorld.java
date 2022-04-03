b192a8a18f3c40bac2fd3e256912123672f36ac5
Deaglusha <deaglusha@gmail.com>
2022-04-03 22:36:10 +0300

    Changed HelloWorld.java

--- src/main/java/HellWorld.java
+++ src/main/java/HellWorld.java
@@ -3,5 +3,6 @@ package main.java;
 public class HellWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is our second commit");
     }
 }
